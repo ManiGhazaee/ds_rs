b01b6461b639454bb099a97761ffd2379c6b0839
ManiGhazaee <hosseinghazaee1@gmail.com>
2024-02-15 19:36:03 +0330

    queue with tests

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,10 @@
-#![allow(dead_code)]
+use ds_rs::Queue;
 
-struct Q<T> {
-    vec: Vec<Option<T>>,
-    size: usize,
-    capacity: usize,
+fn main() {
+    let mut q: Queue<Vec<&str>> = Queue::new(10);
+    q.enq(vec!["v1"]).unwrap();
+    q.enq(vec!["v2"]).unwrap();
+    q.enq(vec!["v3"]).unwrap();
+    q.enq(vec!["v4"]).unwrap();
+    println!("{:?}", q);
 }
-
-impl<T: Clone> Q<T> {
-    fn new(capacity: usize) -> Self {
-        assert!(capacity > 0);
-        Q {
-            vec: vec![None; capacity],
-            size: 0,
-            capacity,
-        }
-    }
-
-    fn is_empty(&self) -> bool {
-        self.size == 0
-    }
-    fn is_full(&self) -> bool {
-        self.size == self.capacity
-    }
-
-    fn enq(&mut self, val: T) -> Result<(), ()> {
-        if !self.is_full() {
-            self.vec.insert(0, Some(val));
-            self.size += 1;
-            Ok(())
-        } else {
-            Err(())
-        }
-    }
-    fn deq(&mut self) -> Result<(), ()> {
-        if !self.is_empty() {
-            self.vec[self.size] = None;
-            self.size -= 1;
-            Ok(())
-        } else {
-            Err(())
-        }
-    }
-
-    fn tail(&self) -> &Option<T> {
-        &self.vec[self.size]
-    }
-    fn head(&self) -> &Option<T> {
-        &self.vec[0]
-    }
-}
-fn main() {}
